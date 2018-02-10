6111c3da17810d11f8de6274678db14faacb8341
Roger Cheng <roger.random@outlook.com>
2018-02-10 13:56:21 -0800

    Using Materialize CSS

--- SGVHAK_Rover/__init__.py
+++ SGVHAK_Rover/__init__.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+import os
 from flask import Flask
 app = Flask(__name__)
+
+# Randomly generated key means session cookies will not be usable across
+# instances. This is acceptable for now but may need changing later.
+app.secret_key = os.urandom(24)
 
 import SGVHAK_Rover.menu
--- SGVHAK_Rover/menu.py
+++ SGVHAK_Rover/menu.py
@@ -1,6 +1,11 @@
 from SGVHAK_Rover import app
-from flask import render_template
+from flask import flash, render_template
 
 @app.route('/')
 def index():
 	return render_template("index.html")
+
+@app.route('/stop_motors')
+def stop_motors():
+  flash("Stop Motor Placeholder","success")
+  return render_template("index.html")
