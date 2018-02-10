d444b862ca6d4bf0644d0d3c4fb8c33f3649bc22
Roger Cheng <roger.random@outlook.com>
2018-02-10 15:22:05 -0800

    Menu placeholders

--- SGVHAK_Rover/menu.py
+++ SGVHAK_Rover/menu.py
@@ -1,11 +1,28 @@
 from SGVHAK_Rover import app
 from flask import flash, render_template
 
-@app.route('/')
-def index():
-	return render_template("index.html")
+class main_menu:
 
-@app.route('/stop_motors')
-def stop_motors():
-  flash("Stop Motor Placeholder","success")
-  return render_template("index.html")
+  @app.route('/')
+  def index():
+    return render_template("index.html")
+
+  @app.route('/stop_motors')
+  def stop_motors():
+    flash("Stop Motor Placeholder","success")
+    return render_template("index.html")
+
+  @app.route('/drive')
+  def drive():
+    flash("Drive placeholder","success")
+    return render_template("index.html")
+
+  @app.route('/chassis_config')
+  def chassis_config():
+    flash("chassis_config placeholder","success")
+    return render_template("index.html")
+
+  @app.route('/chassis_test')
+  def chassis_test():
+    flash("chassis_test placeholder","success")
+    return render_template("index.html")
