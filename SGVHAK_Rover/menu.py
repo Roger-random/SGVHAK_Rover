1f8cc5656983fc0b6196dfa9043d114ebca19a6e
Roger Cheng <roger.random@outlook.com>
2018-02-11 01:46:34 -0800

    Chassis Configuration screen show wheels roughly relative to each other

--- SGVHAK_Rover/menu.py
+++ SGVHAK_Rover/menu.py
@@ -23,8 +23,25 @@ class main_menu:
   @app.route('/chassis_config')
   def chassis_config():
     chassis.loadDist()
+    rows = set()
+    columns = set()
+    # Count the unique X/Y coordinates into columns/rows
+    for wheel in chassis.wheels:
+      rows.add(wheel['y'])
+      columns.add(wheel['x'])
+    # Create a dictionary of dicationaries to hold entries.
+    wheelTable = dict()
+    for row in rows:
+      wheelTable[row] = dict()
+      for column in columns:
+        wheelTable[row][column] = list()
+    # Put each wheel into its matching location in the table.
+    for wheel in chassis.wheels:
+      wheelTable[wheel['y']][wheel['x']].append(wheel)
+
+    # Render table
     return render_template("chassis_config.html",
-      wheels = chassis.wheels)
+      wheelTable = wheelTable)
 
   @app.route('/chassis_test')
   def chassis_test():
