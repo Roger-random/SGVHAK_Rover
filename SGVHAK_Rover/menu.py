cf372ee6faca2cae90a2d0a8888603cca1bcc258
Roger Cheng <roger.random@outlook.com>
2018-04-02 16:54:10 -0700

    Basic support for two-wheel differential drive

--- SGVHAK_Rover/menu.py
+++ SGVHAK_Rover/menu.py
@@ -90,12 +90,21 @@ class main_menu:
       pct_angle = float(request.form['pct_angle'])
       magnitude = float(request.form['magnitude'])
 
-      # TODO: Find a more general way to do this math rather than hard-coding
-      # wheel names.
-      if pct_angle >= 0:
-        radius = chassis.radius_for('front_right', pct_angle)
+      if len(chassis.wheels) == 2:
+        # Presumably two-wheel differential drive
+        if pct_angle == 0:
+          radius = float("inf")
+        elif pct_angle>0:
+          radius = chassis.minRadius + (chassis.maxRadius-chassis.minRadius) * (100-pct_angle)/100.0
+        else:
+          radius = -chassis.minRadius - (chassis.maxRadius-chassis.minRadius) * (100+pct_angle)/100.0
       else:
-        radius = chassis.radius_for('front_left', pct_angle)
+        # TODO: Find a more general way to do this math rather than hard-coding
+        # wheel names.
+        if pct_angle >= 0:
+          radius = chassis.radius_for('front_right', pct_angle)
+        else:
+          radius = chassis.radius_for('front_left', pct_angle)
 
       chassis.move_velocity_radius(magnitude, radius)
 
